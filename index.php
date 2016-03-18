890c4e392d4f93a7341af316f2310ed0c0441a8f
shehrozsheikh <shehrozsheikh@outlook.com>
2016-03-19 02:19:00 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Welcome to Facebook</title>
-
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="noindex">
 	<meta name="googlebot" content="noindex">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" class="showscr"></script>
 
 	<script>
 		$(document).ready(function(){
 			var width = $(window).width();
-			window.alert(width);
+			window.alert(width);/*
 			if(width > 768){
 				window.location = "Desktop/index.html";
 			}
 			else{
 				window.location = "Mobile/index.html";
-			}
+			}*/
 		})
 	</script>
 </head>
