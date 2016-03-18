ae51f99a4ecdb31ac322e3288e0a92504b445331
shehrozsheikh <shehrozsheikh@outlook.com>
2016-03-19 02:10:34 +0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 	<meta name="robots" content="noindex">
 	<meta name="googlebot" content="noindex">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" class="showscr"></script>
 
